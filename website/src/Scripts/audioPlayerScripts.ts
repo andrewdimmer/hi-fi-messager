f238ad67748ecce7d11f453c9f9f1ba7df203aaf
Andrew Dimmer <43498863+andrewdimmer@users.noreply.github.com>
2020-06-14 06:05:59 -0400

    fix(website): Update Audio Mappings (#11)
    We're now using shorter sounds for more common characters to speed things up for the demo.

--- website/src/Scripts/audioPlayerScripts.ts
+++ website/src/Scripts/audioPlayerScripts.ts
@@ -15,24 +15,24 @@ export const initAudio = () => {
     innerHTML += `<audio controls id="L_Audio"><source src="${window.location.href}/assets/audio/217351__vkproduktion__deflating-balloon.wav" type="audio/wav" /></audio>`;
     innerHTML += `<audio controls id="M_Audio"><source src="${window.location.href}/assets/audio/219506__jarredgibb__microwave-bleeps-01.wav" type="audio/wav" /></audio>`;
     innerHTML += `<audio controls id="N_Audio"><source src="${window.location.href}/assets/audio/221642__eggsandwichent__ba-dum-bum-all.wav" type="audio/wav" /></audio>`;
-    innerHTML += `<audio controls id="O_Audio"><source src="${window.location.href}/assets/audio/232927__bmusic92__mac-start-up.wav" type="audio/wav" /></audio>`;
+    innerHTML += `<audio controls id="O_Audio"><source src="${window.location.href}/assets/audio/446000__breviceps__fart-3.wav" type="audio/wav" /></audio>`;
     innerHTML += `<audio controls id="P_Audio"><source src="${window.location.href}/assets/audio/245740__2mbst1__ruelps-18.wav" type="audio/wav" /></audio>`;
     innerHTML += `<audio controls id="Q_Audio"><source src="${window.location.href}/assets/audio/249550__fngersounds__dong1.wav" type="audio/wav" /></audio>`;
     innerHTML += `<audio controls id="R_Audio"><source src="${window.location.href}/assets/audio/258020__kodack__arcade-bleep-sound.wav" type="audio/wav" /></audio>`;
     innerHTML += `<audio controls id="S_Audio"><source src="${window.location.href}/assets/audio/273984__metaepitome__cartoon-sound-02.wav" type="audio/wav" /></audio>`;
     innerHTML += `<audio controls id="T_Audio"><source src="${window.location.href}/assets/audio/276046__obxjohn__cartoon-clearing-head-sound.wav" type="audio/wav" /></audio>`;
     innerHTML += `<audio controls id="U_Audio"><source src="${window.location.href}/assets/audio/277044__bnabors1__a-very-nice-burp.wav" type="audio/wav" /></audio>`;
-    innerHTML += `<audio controls id="V_Audio"><source src="${window.location.href}/assets/audio/345688__inspectorj__comedic-whistle-a.wav" type="audio/wav" /></audio>`;
+    innerHTML += `<audio controls id="V_Audio"><source src="${window.location.href}/assets/audio/386857__bustercherry__squeaky-ringer.wav" type="audio/wav" /></audio>`;
     innerHTML += `<audio controls id="W_Audio"><source src="${window.location.href}/assets/audio/345689__inspectorj__comedic-boing-a.wav" type="audio/wav" /></audio>`;
     innerHTML += `<audio controls id="X_Audio"><source src="${window.location.href}/assets/audio/348791__nebyoolae__toy-ducks-quacking.wav" type="audio/wav" /></audio>`;
     innerHTML += `<audio controls id="Y_Audio"><source src="${window.location.href}/assets/audio/377639__danarobinsondesignsgmailcom__b15.wav" type="audio/wav" /></audio>`;
     innerHTML += `<audio controls id="Z_Audio"><source src="${window.location.href}/assets/audio/384188__inspectorj__rooster-crowing-a.wav" type="audio/wav" /></audio>`;
-    innerHTML += `<audio controls id="SPACE_Audio"><source src="${window.location.href}/assets/audio/386857__bustercherry__squeaky-ringer.wav" type="audio/wav" /></audio>`;
+    innerHTML += `<audio controls id="SPACE_Audio"><source src="${window.location.href}/assets/audio/345688__inspectorj__comedic-whistle-a.wav" type="audio/wav" /></audio>`;
     innerHTML += `<audio controls id="ENTER_Audio"><source src="${window.location.href}/assets/audio/389288__themaggotr__xilofono-sound-wet.wav" type="audio/wav" /></audio>`;
     innerHTML += `<audio controls id="NUM1_Audio"><source src="${window.location.href}/assets/audio/410751__plantmonkey__arrow-strike.wav" type="audio/wav" /></audio>`;
     innerHTML += `<audio controls id="SLASH_Audio"><source src="${window.location.href}/assets/audio/418106__crazymonke9__single-dog-bark-2.wav" type="audio/wav" /></audio>`;
     innerHTML += `<audio controls id="PERIOD_Audio"><source src="${window.location.href}/assets/audio/442906__qubodup__pig-oink.wav" type="audio/wav" /></audio>`;
-    innerHTML += `<audio controls id="COMMA_Audio"><source src="${window.location.href}/assets/audio/446000__breviceps__fart-3.wav" type="audio/wav" /></audio>`;
+    innerHTML += `<audio controls id="COMMA_Audio"><source src="${window.location.href}/assets/audio/232927__bmusic92__mac-start-up.wav" type="audio/wav" /></audio>`;
     innerHTML += `<audio controls id="HTTPS_Audio"><source src="${window.location.href}/assets/audio/203512__blueneon__56k-modem.wav" type="audio/wav" /></audio>`;
     element.innerHTML = innerHTML;
   }
